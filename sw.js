6005408784024432eed3039c8f3f07a72a1c8122
mdshahin2026 <hujifa2014@gmail.com>
2026-06-06 05:44:13 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+// Service Worker for জমা-খরচ হিসাব PWA
+const CACHE_NAME = 'joma-khoroch-v1';
+const urlsToCache = [
+  '/joma-khoroch/',
+  '/joma-khoroch/index.html'
+];
+
+// Install
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch - Network first, then cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
